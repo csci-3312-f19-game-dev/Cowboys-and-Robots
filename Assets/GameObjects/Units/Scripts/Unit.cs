036fa7210a27ee7abb19d7133e2c9e034823e3e4
Logan Bown <lbown@trinity.edu>
2019-10-29 15:18:38 -0500

    Added some structure

---
+++ Assets/GameObjects/Units/Scripts/Unit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Unit : Damageable
+{
+    protected int moveSpeed;
+    protected int range;
+    protected int damage;
+    protected float atkSpd;
+}
