bbd1daf8367814fa175dc4b23706643ba39f4f28
Chase Wilson <cwilson7@trinity.edu>
2019-11-04 11:14:41 -0600

    Added pause menu, unit cost labels

---
+++ Assets/UI Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
