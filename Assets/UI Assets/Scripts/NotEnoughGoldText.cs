96a63fafbc1ba1d08b3f32197e8d8aaafcfa4d9f
Chase Wilson <cwilson7@trinity.edu>
2019-11-05 15:54:13 -0600

    added not enough cash text

--- Assets/UI Assets/Scripts/NotEnoughGoldText.cs
+++ Assets/UI Assets/Scripts/NotEnoughGoldText.cs
@@ -6,28 +6,30 @@ using TMPro;
 
 public class NotEnoughGoldText : MonoBehaviour
 {
-    float duration = 2f, speed = 3f;
-    TMP_Text txt;
+    private float duration = 1.5f, speed = 2.5f, timer = 0;
+
+    public TMP_Text txt;
     // Start is called before the first frame update
     void Start()
     {
-        txt = GetComponent<TMP_Text>();
+       // txt = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         //destroy text after desired duration
-        if(Time.time > duration)
+        if(timer > duration)
         {
-            Destroy(gameObject);
+           Destroy(gameObject);
         }
 
         //move text upwards
         gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + speed);
 
         //make text fade out
-        float ratio = Time.time / duration;
+        float ratio = timer / duration;
         Color col = txt.color;
         col.a = Mathf.Lerp(1, 0, ratio);
         txt.color = col;
