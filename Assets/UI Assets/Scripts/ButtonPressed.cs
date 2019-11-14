f1918d58f19ff4ec36925e684f2acda79c1791e5
Chase Wilson <cwilson7@trinity.edu>
2019-11-14 16:52:59 -0600

    Added info panel

---
+++ Assets/UI Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour
+{
+
+    public GameObject infoPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(ExitTab);
+    }
+
+    void ExitTab()
+    {
+        Debug.Log("Exited");
+        Destroy(infoPanel);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
