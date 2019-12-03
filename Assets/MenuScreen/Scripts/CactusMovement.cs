55f25465feb22055238738c0583f7af78ff1b8eb
kderrig <kderrig@trinity.edu>
2019-12-03 12:28:18 -0600

    menu screen

--- Assets/MenuScreen/Scripts/CactusMovement.cs
+++ Assets/MenuScreen/Scripts/CactusMovement.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class CactusMovement : MonoBehaviour
 {
-    private GameObject cactusOne;
-    private GameObject cactusTwo;
-    private GameObject mountains;
+    private GameObject[] cactusOne;
+    private GameObject[] cactusTwo;
+    private GameObject[] mountains;
     // Start is called before the first frame update
     void Start()
     {
-        cactusOne = GameObject.FindWithTag("cactus");
-        cactusTwo = GameObject.FindWithTag("cactus2");
-        mountains = GameObject.FindWithTag("mountains");
+        cactusOne = GameObject.FindGameObjectsWithTag("cactus");
+        cactusTwo = GameObject.FindGameObjectsWithTag("cactus2");
+        mountains = GameObject.FindGameObjectsWithTag("mountains");
     }
 
     // Update is called once per frame
     void Update()
     {
-        var tmp = cactusOne.transform.position;
-        cactusOne.transform.position = new Vector3((float)(tmp.x - 0.005), tmp.y, tmp.z);
-        //if (cactusOne.transform.position.x < -150) {
-        //    cactusOne.transform.position = new Vector3(0, tmp.y, tmp.z);
-        //};
-        var tmp2 = cactusTwo.transform.position;
-        cactusTwo.transform.position = new Vector3((float)(tmp2.x - 0.003), tmp2.y, tmp2.z);
-        var tmp3 = mountains.transform.position;
-        mountains.transform.position = new Vector3((float)(tmp3.x - 0.002), tmp3.y, tmp3.z);
+        foreach (GameObject i in cactusOne)
+        {
+            var tmp = i.transform.position;
+            i.transform.position = new Vector3((float)(tmp.x - 0.003), tmp.y, tmp.z);
+        }
+        foreach (GameObject i in cactusTwo)
+        {
+            var tmp2 = i.transform.position;
+            i.transform.position = new Vector3((float)(tmp2.x - 0.001), tmp2.y, tmp2.z);
+        }
+        foreach (GameObject i in mountains)
+        {
+            var tmp3 = i.transform.position;
+            i.transform.position = new Vector3((float)(tmp3.x - 0.0007), tmp3.y, tmp3.z);
+        }
     }
+
 }
